e56e13f76ce8122b1ec6981cda6c945c1e7083c3
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-06-09 21:57:17 +0530

    commit

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'newapp.apps.NewappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'newproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +122,10 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+#added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
