2d8079876de9a2d2e093d9ff592d5b7a7c18f2a8
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-06-10 10:45:47 +0530

    commit

--- newproject/newapp/views.py
+++ newproject/newapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.htm')
+    #return(request,HttpResponse('this is home page'))
+def login(request):
+    return render(request,'login.htm')
+    #return(request,HttpResponse('this is home page'))
+def logout(request):
+    return render(request,'logout.htm')
+    #return(request,HttpResponse('this is home page'))
+    
