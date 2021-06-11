e067babf9d406386ed43dd07fa739e4a477c9498
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-06-11 21:37:23 +0530

    commit

--- newproject/newapp/views.py
+++ newproject/newapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,HttpResponse
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -6,6 +8,12 @@ def index(request):
     return render(request,'index.htm')
     #return(request,HttpResponse('this is home page'))
 def login(request):
+    # if request.method=="POST":
+    #     username=request.POST.get("username")
+    #     password=request.POST.get("password")
+
+    #     #check if user has entered correct credentials
+    #     user = authenticate(username=username, password=password)
     return render(request,'login.htm')
     #return(request,HttpResponse('this is home page'))
 def logout(request):
