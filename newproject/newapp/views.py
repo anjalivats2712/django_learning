cf20f674d4616e4e7cfdeb0ef0d55b3134b3fd80
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-06-13 21:21:39 +0530

    commit

--- newproject/newapp/views.py
+++ newproject/newapp/views.py
@@ -1,22 +1,34 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import redirect, render,HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout
+
 
 
 # Create your views here.
 
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render(request,'index.htm')
     #return(request,HttpResponse('this is home page'))
 def login(request):
-    # if request.method=="POST":
-    #     username=request.POST.get("username")
-    #     password=request.POST.get("password")
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
 
-    #     #check if user has entered correct credentials
-    #     user = authenticate(username=username, password=password)
+        #check if user has entered correct credentials
+        user = authenticate(username=username, password=password)
+        if user is not None:
+        # A backend authenticated the credentials
+            return redirect("/")
+        else:
+        # No backend authenticated the credentials
+            return render(request,'login.htm')
     return render(request,'login.htm')
     #return(request,HttpResponse('this is home page'))
-def logout(request):
-    return render(request,'logout.htm')
+def logoutuser(request):
+    logout(request)
+    return redirect("/login")
     #return(request,HttpResponse('this is home page'))
     
