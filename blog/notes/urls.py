7ce456b8410de0c632b7f5e13ed5630b539624f3
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-09-11 22:08:20 +0530

    commit

---
+++ blog/notes/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import PostListView,PostDetailsView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',PostListView.as_view(),name='home'),
+    path('post/<int:pk>/',PostDetailsView.as_view(),name='post_detail')
+]
--- blog/notes/views.py
+++ blog/notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from notes.models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model=Post
+    template_name='index.html'
+    context_object_name='all_posts_list'
+
+class PostDetailsView(DetailView):
+    model=Post
+    template_name='detailview.html'
