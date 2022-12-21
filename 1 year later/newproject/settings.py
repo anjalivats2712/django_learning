2b960135b4d7c71ba6bff21af4500167e9ce8d2b
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2022-12-21 22:58:56 +0530

    commit

--- 1 year later/newproject/settings.py
+++ 1 year later/newproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'homee.apps.HomeeConfig',
     'newapp.apps.NewappConfig',
     'django.contrib.admin',
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
